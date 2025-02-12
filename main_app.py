c5b134e227298c750437af9fc06b6984cbf1b3d6
root <root@kali.localdomain>
2025-02-12 09:13:07 -0500

    second commit

---
+++ main_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import abort, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return redirect(url_for('index'))
+
+@app.route("/profile")
+def index():
+    return render_template('index.html')
